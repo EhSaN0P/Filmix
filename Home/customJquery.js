e8cd94940ba88540fd3333369b1e2c86b7964090
ehsan <ehsanog13@gmail.com>
2025-02-13 19:08:13 +0330

    First Slider Created

--- Home/customJquery.js
+++ Home/customJquery.js
@@ -1,4 +1,31 @@
 $(function () {
-    console.log(324);
+
+// creating first interduce movies slider 
+interduceSldier()
+    
+
+      
+
+
     
 });
+
+
+
+// interduce Sldier
+function interduceSldier() { 
+   
+$(".slider").owlCarousel({
+    items: 1,
+    dots:true,
+    nav:true,
+    loop:true,
+    autoplay:false,
+    smartSpeed:2500,
+    autoplayHoverPause:true
+    
+  });
+
+
+    
+ }
