c09a39d39b429e86bb21d534b2a61fd28210d5d5
ehsan <ehsanog13@gmail.com>
2025-02-13 16:57:06 +0330

    data object added

---
+++ dataVideos-object/data.js
@@ -0,0 +1,66 @@
+export let videos = {
+
+    new:{
+
+    },
+
+    genre:{
+        // horror movies
+            horror:{
+                // counjoring 2
+                    counjoring2 : {
+                        namee: "Counjoring 2",
+                        rate:8,
+                        link: '<iframe src="https://www.aparat.com/video/video/embed/videohash/crd53gz/vt/frame"  allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>'
+                    },
+                    
+                    // it 2
+                    it2 : {
+                        namee: "it 2",
+                        rate:9,
+                        link: '<iframe src="https://www.aparat.com/video/video/embed/videohash/m95ot/vt/frame"  allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>'
+                    },
+                    // grudge2
+                    grudge2 : {
+                        namee: "Grudge 2",
+                        rate:5,
+                        link: '<iframe src="https://www.aparat.com/video/video/embed/videohash/r114220/vt/frame"  allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>'
+                    },
+
+            },
+            // fun movies
+            fun:{
+
+            },
+            // action movies
+            action:{
+
+            },
+
+            // heros movies
+            heros:{
+
+            },
+
+            // romantic movies
+            romantic:{
+
+            },
+            // anime 
+            anime:{
+
+            },
+            // animations
+            animations:{
+
+            },
+
+    },
+    ratest:{
+
+    }
+
+}
+
+
+
