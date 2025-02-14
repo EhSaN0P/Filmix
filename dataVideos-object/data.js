6d1b6d39e940232ddffc1c93b485b1a94d8d74bb
ehsan <ehsanog13@gmail.com>
2025-02-14 13:46:46 +0330

    img added to data

--- dataVideos-object/data.js
+++ dataVideos-object/data.js
@@ -11,20 +11,23 @@ export let videos = {
                     counjoring2 : {
                         namee: "Counjoring 2",
                         rate:8,
-                        link: '<iframe src="https://www.aparat.com/video/video/embed/videohash/crd53gz/vt/frame"  allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>'
+                        link: '<iframe src="https://www.aparat.com/video/video/embed/videohash/crd53gz/vt/frame"  allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>',
+                        img: "../videos img/horror/counjoring 2.jpg",
                     },
                     
                     // it 2
                     it2 : {
                         namee: "it 2",
                         rate:9,
-                        link: '<iframe src="https://www.aparat.com/video/video/embed/videohash/m95ot/vt/frame"  allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>'
+                        link: '<iframe src="https://www.aparat.com/video/video/embed/videohash/m95ot/vt/frame"  allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>',
+                        img: "../../videos img/horror/it-hollywood-movie-mhzcl1vliypxm3gl.jpg",
                     },
                     // grudge2
                     grudge2 : {
                         namee: "Grudge 2",
                         rate:5,
-                        link: '<iframe src="https://www.aparat.com/video/video/embed/videohash/r114220/vt/frame"  allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>'
+                        link: '<iframe src="https://www.aparat.com/video/video/embed/videohash/r114220/vt/frame"  allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>',
+                        img: "../videos img/horror/the-grudge-2-2006-121668.jpg",
                     },
 
             },
